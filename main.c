a92d8b4a0d7b8d349780f362b10aa38aec2ca36b
SeM2x <houssem135.302@gmail.com>
2023-11-14 07:29:33 +0100

    stuff

--- main.c
+++ main.c
@@ -7,16 +7,19 @@
 */
 int main(int argc, char **argv)
 {
-	char command[100];
+	char *command;
 	char **av;
+	size_t buff_size = 100;
 	
+	command = (char *)malloc(buff_size * sizeof(char));
 	if (argc < 0)
 		return (1);
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			print_str("#simple_shell$ ");
-		scan_str(command, 100);
+		scan_str(command, buff_size);
+		printf("%s\n", command);
 		av = format(command, ' ');
 		if (av[0][0] != '\0')
 		{
